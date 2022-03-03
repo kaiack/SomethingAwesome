5bcb2c7c9e26b7dd6a1355b583da7ad52523ad54
Kai <kaisoman56@gmail.com>
2022-03-04 00:08:40 +1100

    basic functionality working for posts

--- project/app.js
+++ project/app.js
@@ -5,6 +5,9 @@ const app = express();
 
 const Post = require('./models/post');
 
+// This allows us to send put, delete etc updates from html forms.
+const methodOverride = require('method-override');
+
 // Options {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}
 mongoose.connect('mongodb://localhost:27017/forum');
 const db = mongoose.connection;
@@ -23,6 +26,7 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.listen(3000, () =>{
     console.log("App listening on port 3000");
@@ -44,7 +48,9 @@ app.get('/posts/new', (req, res) =>{
 })
 
 app.post('/posts', async (req, res) =>{
-    res.send(req.body);
+    const post = new Post(req.body.post);
+    await post.save();
+    res.redirect(`/posts/${post._id}`);
 })
 
 app.get('/posts/:id', async (req, res) =>{
@@ -52,5 +58,24 @@ app.get('/posts/:id', async (req, res) =>{
     //console.log(id);
     const post = await Post.findById(id);
     res.render('posts/show', {post});
+})
+
+app.get('/posts/:id/edit', async (req, res) =>{
+    const {id} = req.params;
+    const post = await Post.findById(id);
+    res.render('posts/edit', {post});
+})
+
+app.put('/posts/:id', async(req, res) =>{
+    // console.log("EDDITEED")
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body.post);
+    res.redirect(`/posts/${req.params.id}`);
+})
+
+app.delete('/posts/:id', async(req, res) =>{
+    console.log("DELETEEETDDD")
+    // res.send("DELETINGHEHEH")
+    await Post.findByIdAndDelete(req.params.id);
+    res.redirect(`/posts`);
 })
 
